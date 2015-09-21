ca1372de786c8664496e2217eae745cf3611e023
andrecadgarcia <andrecadgarcia@gmail.com>
2015-09-21 01:35:14 -0300

    Update DepartmentVO.php

--- php/VO/DepartmentVO.php
+++ php/VO/DepartmentVO.php
@@ -3,7 +3,7 @@ class DepartmentVO {
     protected $id_department, $id_university, $name, $initials;
     
     //contructor creates a new department
-    public function __contruct($id_department, $id_university, $name, $initials;) {
+    public function __contruct($id_department, $id_university, $name, $initials) {
         $this->id_department = $id_department;
         $this->id_university = $id_university; 
         $this->name = $name; 
@@ -20,11 +20,11 @@ class DepartmentVO {
     }
     
     public function setId_university($id_university) {
-        $this->university = $id_university;   
+        $this->id_university = $id_university;   
     }
     
     public function getId_university() {
-        return $this->university;
+        return $this->id_university;
     }
     
     public function setName($name) {
@@ -54,7 +54,7 @@ class DepartmentVO {
 
     //Generate string to send info back to javascript
     public function toString() {
-        $string = $this->getId_department() . "," . $this->getId_university() . "," . $this->getName() . "," .$this->getId_initials();
+        $string = $this->getId_department() . "," . $this->getId_university() . "," . $this->getName() . "," .$this->getInitials();
         return $string;
     }    
     
